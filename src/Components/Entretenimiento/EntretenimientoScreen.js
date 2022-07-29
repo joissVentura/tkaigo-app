194fd89628ea4c807c0c8f3270a9b74e65596d82
Joiss Ventura Rivera <2389790@gmail.com>
2022-07-29 11:42:53 -0500

    Se borraron los archivos innecesarios y se crearon los componentes

---
+++ src/Components/Entretenimiento/EntretenimientoScreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EntretenimientoScreen = () => {
+    return (
+        <div>EntretenimientoScreen</div>
+    )
+}
---
+++ src/Components/Gastronomia/GastronomiaScreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const GastronomiaScreen = () => {
+    return (
+        <div>GastronomiaScreen</div>
+    )
+}
