9353e0cf3455a4c349a758d7f4518d603e44d6d1
Joiss Ventura Rivera <2389790@gmail.com>
2022-08-01 13:50:04 -0500

    Se ha agregado a bootstrap como una dependencia del proyecto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {TKaigoApp} from './TKaigoApp';
 import './index.css';
-import './bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
